24292c8605162c9952c869359eec4311cac61c72
vwen123 <vivianeiwen123@gmail.com>
2026-02-14 14:44:20 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App'; // 注意：这里的 App 必须对应 src 文件夹里的 App.js
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
